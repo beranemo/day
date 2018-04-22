e7e902f6af47975523bb0d0de934640b5f7e8647
beranemo <beranemo@gmail.com>
2018-04-22 10:44:13 +0800

    complete features of admin

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,57 @@
+class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "restaurant was successfully updated"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash.now[:alert] = "restaurant was failed to update"
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
+  end
+
+
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
+end
